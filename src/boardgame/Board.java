7f92bc42bfaf6bae005aa05faca6bec7fd8de5c5
Bob Talinn-W <bobpitaia@gmail.comlsgit config --global user.email bobpitaia@gmail.com>
2023-02-25 06:26:10 -0300

    Chess laying and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,6 +8,22 @@ public class Board {
 
     private  Piece[][] pieces;
 
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
     public Board(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
@@ -15,4 +31,12 @@ public class Board {
 
 
     }
+    public  Piece piece(int row, int column){
+        return pieces[row][column];
+    }
+
+    public  Piece piece(Position position){
+        return pieces [position.getRow()][position.getColumn()];
+    }
+
 }
