9150bc0d9c0ee5e30bc632bd829e93e58cff0261
Bob Talinn-W <bobpitaia@gmail.comlsgit config --global user.email bobpitaia@gmail.com>
2023-02-28 00:11:02 -0300

    Check logics

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -5,21 +5,37 @@ import boardgame.Piece;
 import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
-    private  Color color;
+
+    private Color color;
+    private int moveCount;
+
+    public ChessPiece(Board board, Color color) {
+        super(board);
+        this.color = color;
+    }
 
     public Color getColor() {
         return color;
     }
 
-
-    public ChessPiece(Board board, Color color) {
-        super(board);
-        this.color = color;
-
-
+    public int getMoveCount() {
+        return moveCount;
     }
-    protected boolean isThereOpponentPiece(Position position){
-        ChessPiece p = (ChessPiece)getBoard().piece(position);
-        return p !=null && p.getColor() !=color;
+
+    protected void increaseMoveCount() {
+        moveCount++;
+    }
+
+    protected void decreaseMoveCount() {
+        moveCount--;
+    }
+
+    public ChessPosition getChessPosition() {
+        return ChessPosition.fromPosition(position);
+    }
+
+    protected boolean isThereOpponentPiece(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p != null && p.getColor() != color;
     }
 }
